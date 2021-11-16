bea1c5f6dbfb1ab448180547a467e0eaa735b33d
jmcooper <jim@cooperplanet.com>
2021-11-15 17:51:06 -0700

    Upgrade to v13

--- src/app/services/data-repository.ts
+++ src/app/services/data-repository.ts
@@ -30,10 +30,10 @@ export class DataRepositoryService {
 
   enroll(classId): Observable<any> {
     if (!this.currentUser)
-      return throwError('User not signed in');
+      return throwError(() => new Error('User not signed in'));
 
     if (this.currentUser.classes.includes(classId))
-      return throwError('Already enrolled');
+      return throwError(() => new Error('Already enrolled'));
 
     this.currentUser.classes.push(classId);
 
@@ -42,21 +42,21 @@ export class DataRepositoryService {
 
   drop(classId): Observable<any> {
     if (!this.currentUser)
-      return throwError('User not signed in');
+      return throwError(() => new Error('User not signed in'));
 
     if (!this.currentUser.classes.includes(classId))
-      return throwError('Not enrolled');
+      return throwError(() => new Error('Not enrolled'));
 
-    this.currentUser.classes = this.currentUser.classes.filter(c => c.classId !== classId);
+    this.currentUser.classes = this.currentUser.classes.filter(c => c !== classId);
 
-    return EMPTY.pipe(delay(1000));
+    return EMPTY.pipe(delay(3000));
   }
 
   signIn(credentials): Observable<any> {
     //Never, ever check credentials in client-side code.
     //This code is only here to supply a fake endpoint for signing in.
     if (credentials.email !== 'me@whitebeards.edu' || credentials.password !== 'super-secret')
-      return throwError('Invalid login');
+      return throwError(() => new Error('Invalid login'));
 
     this.currentUser = {
       userId: 'e61aebed-dbc5-437a-b514-02b8380d8efc',
